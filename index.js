e94e4ee9ab7227265d54dfa21c031a077412f99a
rashel9255 <ibrahim2000167@gmail.com>
2025-12-11 06:56:03 +0600

    feat: connect mongoDB database with the server

--- index.js
+++ index.js
@@ -4,15 +4,43 @@ const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 
+// MongoDB
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0ocgkty.mongodb.net/?appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
 
 app.get("/", (req, res) => {
     res.send("Contest Hub Server is running");
 });
+
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
     console.log(`Contest Hub Server is running on port ${port}`);
