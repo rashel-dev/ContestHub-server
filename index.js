0558d62ae69c6bc18cb588ddadc00a257d8ecd18
rashel9255 <ibrahim2000167@gmail.com>
2025-12-25 07:56:27 +0600

    update all contest api to send the approved contest only from the server. delete unnecessary approval status check in client side

--- index.js
+++ index.js
@@ -139,9 +139,12 @@ async function run() {
         //get all contests api
         app.get("/contests", async (req, res) => {
             const query = {};
-            const { email, winnerEmail } = req.query;
+            const { email, status } = req.query;
             if (email) {
                 query.creatorEmail = email;
+            }
+            if (status) {
+                query.approvalStatus = status;
             }
 
             const options = {
@@ -535,14 +538,10 @@ async function run() {
             }
         });
 
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         // console.log("You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
     }
 }
 run().catch(console.dir);
